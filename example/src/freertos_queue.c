656e67b3aad3bc3bdd1809d923d455445b59dd6a
Anders Reiche <andersreiche@gmail.com>
2018-03-01 17:31:06 +0100

    queue us working and some simple tests like full and empty queue scenarios has been tested.

--- example/src/freertos_queue.c
+++ example/src/freertos_queue.c
@@ -3,9 +3,10 @@
 #include "task.h"
 #include "queue.h"
 
-#define txPrio 1
+#define txPrio 2
 #define rxPrio 1
 #define QUEUESIZE 2
+#define TIMEOUT 10
 
 xQueueHandle queue_handle = NULL;
 
@@ -13,16 +14,22 @@ void tx(void *p) {
 	int item = 0;
 	while (1) {
 		printf("tx() send to queue : ");
-		long ok = xQueueSend(queue_handle, &item, 500);
+		// Sender item til queue, hvis der ikke er plads i queue failer den efter timeout
+		long ok = xQueueSend(queue_handle, &item, TIMEOUT);
 		puts(ok ? "OK" : "FAILED");
-		vTaskDelay(200);
+		vTaskDelay(50);
 	}
 }
 
 void rx(void *p) {
 	int item = 0;
 	while (1) {
-
+		// Recieve vil vente på at der kommer et item i queue indtil timout, hvorefter den failer
+		if (xQueueReceive(queue_handle, &item, TIMEOUT)) {
+			printf("rx() Got item from queue\n");
+		} else {
+			printf("rx() failed to get an item within %d ticks\n", TIMEOUT);
+		}
 	}
 }
 
